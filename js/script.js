b01ce1013955ae986a1a41167b3938dffd9b6a26
mithun <mithunf@qburst.com>
2016-08-25 18:05:38 +0530

    updated video part

--- js/script.js
+++ js/script.js
@@ -1,6 +1,32 @@
+/*$('.nav').click(function(){
+	$('#mySidenav').css("width","250px");
+	$('.nav-closepad').css("width","320px");
+});
+
+
+
+$('.nav-closepad').click(function(){
+	$('#mySidenav').css("width","0px");
+	$('.nav-closepad').css("width","0px")
+});
+
+$('.new-articles').click(function(){
+	$('#articles-divopen').css("width","250px");
+	$('.articles-closepad').css("width","320px");
+});
+
+
+$('.articles-closepad').click(function(){
+	$('#articles-divopen').css("width","0px");
+	$('.articles-closepad').css("width","0px")
+});
+*/
+
+
+
 $(document).ready(function(){
     var flag=true;
-    $(".header-nav").click(function(){
+    $(".nav").click(function(){
         $("#mySidenav").animate({left: '0px'},"fast");
         $(".nav-closepad").animate({left: '0px'},"fast");
     });
